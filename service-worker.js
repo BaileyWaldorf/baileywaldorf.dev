c548997ca2940c330e4a833eb8912ece26b9f71a
Bailey Waldorf <33463021+BaileyWaldorf@users.noreply.github.com>
2019-09-02 13:23:10 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25129f0889eaed8c6aec3a0fa6ea0e0b.js"
+  "/precache-manifest.74c2acd2b3f39e146aa4c564c679b8e0.js"
 );
 
 workbox.clientsClaim();
