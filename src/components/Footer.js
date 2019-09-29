b97af12911cc1fb4ea97d2128163e705ca30c72e
Bailey Waldorf <33463021+BaileyWaldorf@users.noreply.github.com>
2019-09-29 01:48:43 -0400

    fixed typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../css/App.css';
 const Footer = () => {
     return (
         <div className="footer">
-            No bootsrap, no libraries. Handcoded with ♥ and a keyboard.
+            No bootstrap, no libraries. Handcoded with ♥ and a keyboard.
             <div className="footer-subtext">
                 Bailey Waldorf © 2019 All Rights Reserved
             </div>
